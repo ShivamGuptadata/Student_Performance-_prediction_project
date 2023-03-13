b864085d92e03bd4ccf8861ca812779b5ef89c0b
Shivam Gupta <shivamguptadata@gmail.com>
2023-03-13 17:09:17 +0530

    Data Ingestion

--- src/logger.py
+++ src/logger.py
@@ -10,10 +10,7 @@ LOG_FIlE_PATH=os.path.join(logs_path,LOG_FIlE)
 
 logging.basicConfig(
     filename=LOG_FIlE_PATH,
-    format="[%(asctime)s ] %(lineno)d %(name)s - %(levelname)s -%(message)s",
+    format="[ %(asctime)s ] %(lineno)d %(name)s - %(levelname)s -%(message)s",
     level=logging.INFO,
 
 )
-
-if __name__=="__main__":
-    logging.info("Logging has started")
